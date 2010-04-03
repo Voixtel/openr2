13a5d34b2588763e2cea5da4fbbc619bf526c456
moy <moy@ca512dd4-9257-11dd-be28-810eacef42c0>
2010-04-03 15:26:28 +0000

    created 1.3.0 tag

---
+++ release-1/src/openr2/r2utils.h
@@ -0,0 +1,40 @@
+/*
+ * OpenR2 
+ * MFC/R2 call setup library
+ *
+ * Moises Silva <moy@sangoma.com>
+ * Copyright (C) 2008 Moises Silva
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef _OPENR2_UTILS_H_
+#define _OPENR2_UTILS_H_
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+#define OR2_LIB_INTERFACE 3
+#define OR2_LIB_REVISION 0
+#define OR2_LIB_AGE 0
+const char *openr2_get_version(void);
+const char *openr2_get_revision(void);
+int openr2_strncasecmp(const char *s1, const char *s2, size_t n);
+
+#if defined(__cplusplus)
+} /* endif extern "C" */
+#endif
+
+#endif /* endif defined _OPENR2_UTILS_H_ */
