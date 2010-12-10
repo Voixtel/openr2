ed7646796dead1b444972ac9c406e3b0e7b3c2bf
moy <moy@ca512dd4-9257-11dd-be28-810eacef42c0>
2010-12-10 17:56:52 +0000

    merged 344:349 from trunk

--- src/r2proto.c
+++ src/r2proto.c
@@ -821,6 +821,7 @@ static void open_logfile(openr2_chan_t *r2chan, int backward)
 		EMI(r2chan)->on_os_error(r2chan, myerrno);
 		openr2_log(r2chan, OR2_CHANNEL_LOG, OR2_LOG_ERROR, "fopen failed: %s\n", strerror(myerrno));
 	} else {
+		EMI(r2chan)->on_call_log_created(r2chan, r2chan->logname);
 		currtime = time(NULL);
 		if (openr2_ctime_r(&currtime, timestr)) {
 			timestr[strlen(timestr)-1] = 0; /* remove end of line */
@@ -909,7 +910,7 @@ static void handle_incoming_call(openr2_chan_t *r2chan)
 		return;
 	}
 	/* notify the user that a new call is starting to arrive */
-	EMI(r2chan)->on_call_init(r2chan, r2chan->logname);
+	EMI(r2chan)->on_call_init(r2chan);
 }
 
 static void mf_fwd_safety_timeout_expired(openr2_chan_t *r2chan)
@@ -927,6 +928,7 @@ static void prepare_mf_tone(openr2_chan_t *r2chan, int tone)
 	if (!tone && r2chan->mf_write_tone) {
 		openr2_log(r2chan, OR2_CHANNEL_LOG, OR2_LOG_MF_TRACE, "MF Tx >> %c [OFF]\n", r2chan->mf_write_tone);
 		if (openr2_io_flush_write_buffers(r2chan)) {
+			openr2_log(r2chan, OR2_CHANNEL_LOG, OR2_LOG_ERROR, "failed to flush tx buffers\n");
 			return;
 		}
 	} 
@@ -948,7 +950,7 @@ static void prepare_mf_tone(openr2_chan_t *r2chan, int tone)
 			}
 		}	
 		r2chan->mf_write_tone = tone;
-	}	
+	}
 }
 
 /* this function just accepts from -3 to 1 as valid offsets */
